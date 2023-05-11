55353824076837ac4065806a3521b85c1348bc00
woohaen88 <woohaen88@gmail.com>
2023-05-11 03:19:34 +0000

    CRUD test 완료

--- campings/tests/test_model_camping_create_with_tag.py
+++ campings/tests/test_model_camping_create_with_tag.py
@@ -10,7 +10,7 @@ from tags.models import Tag
 CampGroundURL = "http://localhost:8000/api/v1/camping/"
 
 BASE_PAYLOAD = dict(
-    check_in="2022-12-21",
+    check_in="2023-12-21",
     check_out="2023-12-23",
     ratings=3,
     description="",
@@ -18,6 +18,10 @@ BASE_PAYLOAD = dict(
     name="sample",
     tags=[1, 2],
 )
+
+
+def detail_url(campgroud_id: str) -> str:
+    return f"{CampGroundURL}{campgroud_id}/"
 
 
 def create_user(**kwargs):
@@ -122,19 +126,78 @@ class PrivateCRUDCamping(TestCase):
 
     def test_update_camping_with_tag_success(self):
         """업데이트 하면 태그도 같이 나와야함"""
-        pass
+        campground = create_campground(self.user)
+        url = detail_url(campgroud_id=campground.id)
+
+        PAYLOAD = BASE_PAYLOAD.copy()
+        PAYLOAD["address"] = "update address"
+        PAYLOAD["ratings"] = 4
+
+        res = self.client.put(url, PAYLOAD, format="json")
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        camp = CampGround.objects.get(owner=self.user)
+        date_keys = ["check_in", "check_out"]
+        tag_keys = ["tags"]
+        others_keys = ["ratings", "description", "address", "name"]
+
+        for date_key in date_keys:
+            self.assertEqual(
+                getattr(camp, date_key).strftime("%Y-%m-%d"), PAYLOAD.get(date_key)
+            )
+        for tag_key in tag_keys:
+            self.assertEqual(len(camp.tags.all()), len(PAYLOAD.get("tags")))
+            for tag_item, payload_item in zip(camp.tags.all(), PAYLOAD.get("tags")):
+                self.assertEqual(tag_item.id, payload_item)
+        for others_key in others_keys:
+            self.assertEqual(getattr(camp, others_key), PAYLOAD.get(others_key))
 
     def test_update_camping_with_tag_success(self):
         """
-        업데이트 하면 태그도 같이 나와야함
         업데이트 할 때 체크인 날짜 >= 체크아웃 날짜 --> 400 error
         """
-        pass
+
+        campground = create_campground(self.user)
+        url = detail_url(campgroud_id=campground.id)
+        PAYLOAD = BASE_PAYLOAD.copy()
+
+        # check_in 이 check_out 보다 뒤 일 경우
+        PAYLOAD["check_in"] = "2022-11-21"
+        PAYLOAD["check_out"] = "2022-11-18"
+
+        res = self.client.put(url, PAYLOAD, format="json")
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_partial_update_camping_with_tag_success(self):
         """부분업데이트 태그도 같이 나와야함"""
-        pass
+        campground = create_campground(self.user)
+        url = detail_url(campgroud_id=campground.id)
+
+        PAYLOAD = {
+            "address": "update address",
+            "ratings": 4,
+        }
+
+        res = self.client.patch(url, PAYLOAD, format="json")
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        camp = CampGround.objects.get(owner=self.user)
+
+        self.assertEqual(getattr(camp, "check_in").strftime("%Y-%m-%d"), "2023-12-21")
+        self.assertEqual(getattr(camp, "check_out").strftime("%Y-%m-%d"), "2023-12-23")
+        self.assertEqual(getattr(camp, "ratings"), 4)
+        self.assertEqual(getattr(camp, "description"), "")
+        self.assertEqual(getattr(camp, "address"), "update address")
+        self.assertEqual(getattr(camp, "name"), "sample")
+
+        self.assertEqual(len(camp.tags.all()), len([1, 2]))
+        for tag_item, payload_item in zip(camp.tags.all(), [1, 2]):
+            self.assertEqual(tag_item.id, payload_item)
 
     def test_delete_camping_with_tag_success(self):
         """캠핑 컨텐츠 삭제"""
-        pass
+        campground = create_campground(self.user)
+        url = detail_url(campgroud_id=campground.id)
+
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
