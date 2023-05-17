bf2e4f7524469e4bd468c3323f20830349701513
woohaen88 <woohaen88@gmail.com>
2023-05-17 01:15:41 +0000

    Comment 추가

--- config/settings.py
+++ config/settings.py
@@ -175,4 +175,4 @@ SPECTACULAR_SETTINGS: Dict[str, Any] = {
 
 CORS_ORIGIN_WHITELIST = ["http://127.0.0.1:3000", "http://localhost:3000"]
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True # django가 javascript를 통해 credential를 받아도된다고 허락
